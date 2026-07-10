39fdd2502d1b463f8010cce29fa19cb61b010b63
Jessica Billingslea <42525289+JessB1202@users.noreply.github.com>
2026-07-10 10:52:57 -0400

    Update streamlit_app.py
    Removing more spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,7 @@ import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write(
-  """Choose the fruits you want in your custom Smoothie!
-  """)
+st.write("Choose the fruits you want in your custom Smoothie!")
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write("The name on your Smoothie will be:", name_on_order)
